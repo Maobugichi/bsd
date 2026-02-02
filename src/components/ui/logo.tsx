f40797e4ad2a7e73a9a85820d440ef227d286e51
Maobugichi <immansamuel846@gmail.com>
2026-02-02 17:19:53 +0100

    chore:adjust srcset for logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,3 +1,14 @@
-import logo from "../../assets/logo.webp"
+import logo84 from "../../assets/logo84.webp";
+import logo168 from "../../assets/logo168.webp";
+import logo200 from "../../assets/logo200.webp";
 
-export const Logo = () => (<img className='h-10 w-12 md:h-20  md:w-42' src={logo} loading="lazy" alt='bsd logo'/>)
+export const Logo = () => (
+  <img
+    src={logo84} 
+    srcSet={`${logo84} 84w, ${logo168} 168w, ${logo200} 200w`}
+    sizes="(min-width: 768px) 168px, (min-width: 1024px) 200px, 84px"
+    className="h-10 w-12 md:h-20 md:w-42"
+    alt="bsd logo"
+    loading="lazy"
+  />
+);
