a0e4422f57db088858f9944975a14d96f503ae95
Maobugichi <immansamuel846@gmail.com>
2026-07-24 10:22:59 +0100

    add more proof of work photos

--- src/constants/gallery.constant.ts
+++ src/constants/gallery.constant.ts
@@ -7,7 +7,18 @@ import slide6 from "../assets/bsd-8.jpeg"
 import slide7 from "../assets/bsd-9.jpeg"
 import slide8 from "../assets/bsd-0.jpeg"
 import slide9 from "../assets/bsd-11.jpeg"
-
+import slide0 from "../assets/airla-ltd.png"
+import slide11 from "../assets/cancer-center.png"
+import slide12 from "../assets/cloakroom.png"
+import slide13 from "../assets/diesel-storage.png"
+import slide14 from "../assets/epoxy.png"
+import slide15 from "../assets/firefighter.png"
+import slide16 from "../assets/nbc-port.png"
+import slide17 from "../assets/power-panel.png"
+import slide18 from "../assets/rehabilitation.png"
+import slide19 from "../assets/road.png"
+import slide20 from "../assets/seasoning.png"
+import slide21 from "../assets/unilev.png"
 
 
 
@@ -21,6 +32,18 @@ export const slides = [
     { id: 7, image: slide7, bg: '#FDEBDD' },
     { id: 8, image: slide8, bg: '#DCF0F2' },
     { id: 9, image: slide9, bg: '#FDEBDD' },
+     { id: 0, image: slide0, bg: '#FDEBDD' },
+    { id: 11, image: slide11, bg: '#DCF0F2' },
+    { id: 12, image: slide12, bg: '#FDEBDD' },
+    { id: 13, image: slide13, bg: '#DCF0F2' },
+    { id: 14, image: slide14, bg: '#FDEBDD' },
+    { id: 15, image: slide15, bg: '#DCF0F2' },
+    { id: 16, image: slide16, bg: '#FDEBDD' },
+    { id: 17, image: slide17, bg: '#DCF0F2' },
+    { id: 18, image: slide18, bg: '#FDEBDD' },
+    { id: 19, image: slide19, bg: '#FDEBDD' },
+    { id: 20, image: slide20, bg: '#DCF0F2' },
+    { id: 21, image: slide21, bg: '#FDEBDD' },
   ];
 
   
